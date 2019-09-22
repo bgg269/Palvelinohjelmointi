465d243fc2153d90ce31c29f7d9f63b6317d15f7
unknown <bgg269@myy.haaga-helia.fi>
2019-09-22 13:35:50 +0300

    chapter 5 teht1

--- Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,44 @@
 package com.example.Bookstore;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.CategoryRepository;
+
+
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			crepository.save(new Category("Horror"));
+			crepository.save(new Category("Fantasy"));
+
+			
+			brepository.save(new Book("John's", 2018, "john", 1234, 23, crepository.findByName("Horror").get(0)));
+			brepository.save(new Book("Katy's", 1989, "kate", 5678, 12, crepository.findByName("Fantasy").get(0)));	
+			
+			log.info("fetch all books");
+			for (Book books : brepository.findAll()) {
+				log.info(books.toString());
+			}
+
+		};
+	}
 }
--- Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
+++ Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,10 +1,97 @@
 package com.example.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
 public class Book {
+	@Id 
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	private String title;
 	private long year;
 	private String author;
 	private long isbn;
 	private long price;
+	
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "categoryid")
+    private Category category;
+	
+	public Book() {
+		super();
+	}
+	
+	public Book(String title, long year, String author, long isbn, long price, Category category) {
+		this.title = title;
+		this.year = year;
+		this.author = author;
+		this.isbn = isbn;
+		this.price = price;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public long getYear() {
+		return year;
+	}
+	public void setYear(long year) {
+		this.year = year;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setDepartment(Category category) {
+		this.category = category;
+	}
+	
+	@Override
+	public String toString() {
+		if (this.category != null)
+			return "Book [id=" + id + ", isbn=" + isbn + ", title=" + title + ", author=" + author + " category =" + this.getCategory() + "]";		
+		else
+			return "Book [id=" + id + ", isbn=" + isbn + ", title=" + title + ", author=" + author + "]";
+	}
 
 }
+
